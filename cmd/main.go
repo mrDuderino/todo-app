d74579e821dc3d1b760fb6bbf577f6b34fde1d9f
Vladimir <capcapmor@gmail.com>
2023-03-11 17:52:42 +0300

    dependency injections

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/mrDuderino/todo-app"
 	"github.com/mrDuderino/todo-app/pkg/handler"
+	"github.com/mrDuderino/todo-app/pkg/repository"
+	"github.com/mrDuderino/todo-app/pkg/service"
 )
 
 func main() {
 
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
